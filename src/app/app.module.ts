b79134063eb49c98d0f0bac69a0d296c17f9809d
unknown <armintoussi@outlook.com>
2018-05-21 17:06:03 -0700

    finished up by adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,24 @@ import { NgModule }      from '@angular/core';
 import { AppComponent }        from './app.component';
 import { MarketComponent }     from './market/market.component';
 import { CollectionComponent } from './collection/collection.component';
+import { HeaderComponent }     from './header.component';
 
 //services 
 import { CollectableService } from './shared/collectable.service';
+
+import { routing } from './app.routing';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MarketComponent,
-    CollectionComponent
+    CollectionComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [
     CollectableService
